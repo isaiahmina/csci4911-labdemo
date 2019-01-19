49974bc323d7eb1785a9df50cec3e7fc74386f55
Isaiah Mina <isaiahkgm@gmail.com>
2019-01-18 18:25:01 -1000

    a

--- hello.java
+++ hello.java
@@ -1,1 +1,12 @@
-System.out.Println("Hello");
+
+public class hello {
+
+    public static void main(String[] args) {
+        String msg = "Hello";
+        greeting(msg);
+    }
+    
+    public static void greeting(String msg){
+        System.out.println(msg);
+    }
+}
